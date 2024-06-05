e58d1be01a57469ac7da908b504023bb2ba82101
jayden220 <jaydenvanrooyen87@gmail.com>
2024-06-05 09:33:08 +0200

    final

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 let input = document.querySelectorAll('input')[0]
 let convert = document.querySelectorAll('button')[0]
 let answer = document.getElementsByClassName('days')[0]
+let reset = document.getElementsByClassName('reset')[0]
+
 
 convert.addEventListener('click', convertVal)
 
@@ -9,3 +11,12 @@ function convertVal() {
    let days = input.value * 356 
    answer.innerHTML = `${days} days`
 }
+
+
+function clear(){
+   answer.innerHTML = ''
+   input.value = ''
+}
+
+reset.addEventListener('click', clear)
+
